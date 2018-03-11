2d03b51c842b31f6fb883d4be4e3948f67306297
Bosa100 <salcbrau@my.dom.edu>
2018-03-11 23:21:23 +0000

    added code to parse json response

--- Website/app.py
+++ Website/app.py
@@ -13,9 +13,11 @@ def cake():
 @app.route('/json')
 def getJson():
     import json
-    import urllib.request 
-    res = urllib.request.urlopen("http://10.0.192.62:3000")
-    json_str = res.read()
+    import urllib.request
+    res = urllib.request.urlopen("http://10.0.192.17")
+    data_dict = json.loads(res.read().decode('utf-8'))
+    temp = data_dict["temperature"]
+    return temp
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
