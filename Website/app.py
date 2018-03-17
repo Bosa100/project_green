034f61cdab0c64498ad68cae8a40f6e4f76ffdf3
Bosa100 <salcbrau@my.dom.edu>
2018-03-17 23:02:06 +0000

    added data display pages, created simple database to test

--- Website/app.py
+++ Website/app.py
@@ -1,24 +1,47 @@
 from flask import Flask, render_template
+import json
+import urllib.request
+import sqlite3
 
 app = Flask(__name__)
+
+db = sqlite3.connect("/home/pi/project_green/Database/database.db")
+c = db.cursor()
+
+def getJson(ip):
+    res = urllib.request.urlopen(ip)
+    data_dict = json.loads(res.read().decode('utf-8'))
+    #temp = data_dict["moisture"]
+    #jsonString = res.read()
+    return data_dict
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/cakes')
+@app.route('/demo')
 def cake():
-    return 'Yummy cakes!'
+    return render_template('demo_start.html')
 
-@app.route('/json')
-def getJson():
-    import json
-    import urllib.request
-    res = urllib.request.urlopen("http://10.0.192.163")
-    #data_dict = json.loads(res.read().decode('utf-8'))
-    #temp = data_dict["moisture"]
-    jsonString = res.read()
-    return jsonString
+@app.route('/demo/moisture/<num>/<address>')
+def sensor(num, address):
+    url = "http://" + address
+    data_dict = getJson(url)
+    moisture = data_dict["moisture"]
+    c.execute("INSERT INTO moisture" + num + "(moisture)", (moisture))
+    return render_template('data.html', data = moisture)
+
+
+
+@app.route('/demo/sensor6')
+def sensor6():
+    data_dict = getJson("http://10.0.192.165")
+    return render_template('data.html')
+
+@app.route('/test')
+def test():
+    name = "Jose"
+    return render_template('data.html', word = name)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
