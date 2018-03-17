af6d13f28c4182beb12498a0bac4b9b34d9b31a2
Bosa100 <salcbrau@my.dom.edu>
2018-03-17 19:52:50 +0000

    worked on intro page

--- Website/app.py
+++ Website/app.py
@@ -14,10 +14,11 @@ def cake():
 def getJson():
     import json
     import urllib.request
-    res = urllib.request.urlopen("http://10.0.192.17")
-    data_dict = json.loads(res.read().decode('utf-8'))
-    temp = data_dict["temperature"]
-    return temp
+    res = urllib.request.urlopen("http://10.0.192.163")
+    #data_dict = json.loads(res.read().decode('utf-8'))
+    #temp = data_dict["moisture"]
+    jsonString = res.read()
+    return jsonString
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
