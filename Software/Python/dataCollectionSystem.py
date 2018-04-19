736e6e3d96789870ae15c07f92fd7695dbb52a4e
Bosa100 <salcbrau@my.dom.edu>
2018-04-19 06:31:31 +0000

    created data collection system

--- Software/Python/dataCollectionSystem.py
+++ Software/Python/dataCollectionSystem.py
@@ -1,1 +1,13 @@
-from getJson import get
+from getJson import *
+
+db = sqlite3.connect("/home/pi/project_green/Database/GreenhouseSensors")
+c = db.cursor()
+for i in range(len(ips)):
+	if i == 0:
+		type = 'th'
+	elif i == 1:
+		type = 'm'
+	elif i == 2:
+		type == 'l'
+	for j in range(len(ips[i]):
+		send(ips[i][j], type) 
